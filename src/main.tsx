14965e6472bcc26c079fe7c7b5f36d7d07ef7b2a
blessed-barakasaiba <barakasaiba01@gmail.com>
2026-02-22 16:48:04 +0300

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,13 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 
+const basename = import.meta.env.PROD
+  ? "/ruhuwiko-secondary-school"
+  : "/"; // use "/" locally
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-   <BrowserRouter basename="/ruhuwiko-secondary-school">
+   <BrowserRouter basename={basename}>
     <App />
    </BrowserRouter> 
   </StrictMode>,
